5e48c69d8b4a2d37885750bfe2872c4f8019c56e
newsyntaxcomputer-svg <newsyntaxcomputer@gmail.com>
2026-04-26 17:20:40 +0545

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    const nav = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+};
